cf16deb030368ba9bc44b7a583695c7000ef4bbc
Jeffrey Daniel <jdaniel@uoguelph.ca>
2019-07-12 15:14:45 -0400

    Add example data

---
+++ R/data.R
@@ -0,0 +1,27 @@
+#' Example spatial covariate data
+#'
+#' Simulated example spatial covariate data.
+#'
+#' @format A list of pixel images containing simulated spatial covariates.
+"exdata"
+
+#' Simulated inhomogeneous Poisson point pattern
+#'
+#' Simulated inhomogeneous Poisson point pattern.
+#'
+#' @format An object of class \code{ppp}.
+"Xp"
+
+#' Simulated inhomogeneous Strauss point pattern
+#'
+#' Simulated inhomogeneous Strauss point pattern.
+#'
+#' @format An object of class \code{ppp}.
+"Xs"
+
+#' Simulated inhomogeneous Geyer point pattern
+#'
+#' Simulated inhomogeneous Geyer point pattern.
+#'
+#' @format An object of class \code{ppp}.
+"Xg"
